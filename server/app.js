fe9f01a2a654882ca05c14ed3b2b092957635f78
EddieWebDev <eddandersson@gmail.com>
2023-01-17 13:30:56 +0100

    changed names of folders

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 
-app.use("/api/users", authenticateToken, userRoutes);
+app.use("/api/users", /* authenticateToken, */ userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", authRoutes);
