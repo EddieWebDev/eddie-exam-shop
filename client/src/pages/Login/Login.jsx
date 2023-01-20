76e0b43181334bb08e760c00d2174c560d331ea4
Eddie Andersson <eddandersson@gmail.com>
2023-01-20 22:27:32 +0100

    added google login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,14 +2,24 @@ import { useContext } from "react";
 import { LoginForm } from "../../components/LoginForm";
 import { LogoutButton } from "../../components/LogoutButton";
 import { UserContext } from "../../context/UserContext";
+import { GoogleLogin } from "./components/googleLogin";
 
 const Login = () => {
   const { user } = useContext(UserContext);
 
   return (
-    <section>   
+    <section>
       <h1>{user ? "Log out" : "Log in"}</h1>
-      <div>{user ? <LogoutButton /> : <LoginForm />}</div>
+      <div>
+        {user ? (
+          <LogoutButton />
+        ) : (
+          <div>
+            <LoginForm />
+            <GoogleLogin />
+          </div>
+        )}
+      </div>
     </section>
   );
 };
